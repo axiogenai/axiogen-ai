6e0b6b143ae8af2d64381e3efdac63023d83ca2c
axiogenai <adityaminchekarfx@gmail.com>
2026-05-28 21:06:10 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        login: resolve(__dirname, 'login.html'),
       },
     },
   },
